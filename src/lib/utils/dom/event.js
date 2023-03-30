5d4e0052cf0211726ce98d5423f4680aa32849cf
ahong <1002363357@qq.com>
2023-03-30 11:57:09 +0800

    perf：优化 event.js

--- src/lib/utils/dom/event.js
+++ src/lib/utils/dom/event.js
@@ -1,26 +1,26 @@
-const SUPPORT = {
-    // 是否支持 touch 触摸事件
-    touch: (window.navigator.maxTouchPoints > 0) || 'ontouchstart' in window,
+// 是否支持 touch 触摸事件
+const SUPPORT_TOUCH = (window.navigator.maxTouchPoints > 0) || 'ontouchstart' in window;
 
-    // 是否支持事件监听器参数 passive
-    // 提升移动端性能、告诉浏览器不想阻止事件的默认行为（listener 永远不会调用 preventDefault 方法）
-    passiveListener: (function () {
-        let support = false;
-        try {
-            let options = Object.defineProperty({}, 'passive', {
-                get() {
-                    support = true;
-                }
-            });
-            window.addEventListener('test', null, options);
-            window.removeEventListener('test', null, options);
-        } catch (e) {
-            /* no support：不支持 Object.defineProperty 或 addEventListener、removeEventListener */
-        }
-        return support;
-    }())
-};
-const PASSIVE_EVENTS = ['touchstart', 'touchmove'];
+// 是否支持事件监听器参数 passive
+// 提升移动端性能、告诉浏览器不想阻止事件的默认行为（listener 永远不会调用 preventDefault 方法）
+const SUPPORT_TOUCH_PASSIVE_LISTENER = (function () {
+    let support = false;
+    try {
+        let options = Object.defineProperty({}, 'passive', {
+            get() {
+                support = true;
+            }
+        });
+        window.addEventListener('test', null, options);
+        window.removeEventListener('test', null, options);
+    } catch (e) {
+        /* no support：不支持 Object.defineProperty 或 addEventListener、removeEventListener */
+    }
+    return support;
+}());
+
+// 需要 passive 的 touch 事件
+const TOUCH_PASSIVE_EVENTS = ['touchstart', 'touchmove'];
 
 // 处理多个事件
 function handleMultipleEvent(fn, target, types, listener, options) {
@@ -42,7 +42,7 @@ export function on(target, type, listener, options) {
     }
     if (options === void 0) {
         // 默认使用冒泡事件监听器、支持 passive 将其设为 true
-        if (SUPPORT.passiveListener && PASSIVE_EVENTS.indexOf(type) > -1) {
+        if (SUPPORT_TOUCH_PASSIVE_LISTENER && TOUCH_PASSIVE_EVENTS.indexOf(type) > -1) {
             options = {
                 passive: true,
                 capture: false,
@@ -62,7 +62,7 @@ export function on(target, type, listener, options) {
  * @param {Boolean} options 与事件处理函数相关的可选参数对象
  */
 export function off(target, type, listener, options) {
-    if (isArray(type)) {
+    if (Array.isArray(type)) {
         return handleMultipleEvent(off, target, type, listener, options);
     }
     if (options === void 0) {
@@ -80,7 +80,7 @@ export function off(target, type, listener, options) {
  * @param {Boolean|Object} options 与事件处理函数相关的可选参数对象
  */
 export function one(target, type, listener, options) {
-    if (isArray(type)) {
+    if (Array.isArray(type)) {
         return handleMultipleEvent(one, target, type, listener, options);
     }
     on(target, type, function () {
